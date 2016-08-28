4e2b377d581ae0cb21506b4294715596c03411ee
Devin "GIT SMASH!" Jackson <jackson.t.devin@gmail.com>
2016-08-28 14:38:43 -0400

    simple pie chart
    pie chart with d3 js version 3

---
+++ js/simple_pie.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+
+var data = [
+    {'population':1200,
+     'age':15
+    },
+    {'population':300,
+     'age':35
+    },
+    {'population':800,
+     'age':67
+    },
+    {'population':2300,
+     'age':87
+    },
+    {'population':100,
+     'age':8
+    },
+    {'population':1600,
+     'age':12
+    },
+    {'population':945,
+     'age':44
+    }
+];
+
+var width = 960,
+    height = 500,
+    radius = Math.min(width, height) / 2;
+
+var color = d3.scale.ordinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return d.population; });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+
+var g = svg.selectAll(".arc")
+    .data(pie(data))
+  .enter().append("g")
+    .attr("class", "arc");
+
+g.append("path")
+    .attr("d", arc)
+    .style("fill", function(d) { return color(d.data.age); });
+
+g.append("text")
+    .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+    .attr("dy", ".35em")
+    .style("text-anchor", "middle")
+    .text(function(d) { return "Age: " + d.data.age; });
+
+});
